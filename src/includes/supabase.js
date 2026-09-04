29f9e8e60f055fb0fe9ed2f80c3b3e0cda72bec5
Oberlin <oberlin.keyanfe@facsciences-uy1.cm>
2026-09-04 20:52:04 +0100

    authentification and storage with supabase

--- src/includes/supabase.js
+++ src/includes/supabase.js
@@ -2,7 +2,7 @@ import { createClient } from "@supabase/supabase-js";
 
 // Remplacez ces deux valeurs par celles de VOTRE projet Supabase
 // (Dashboard Supabase -> Project Settings -> API)
-const supabaseUrl = "https://epszxrktalpvklfvhqnk.supabase.co/rest/v1/";
+const supabaseUrl = "https://epszxrktalpvklfvhqnk.supabase.co";
 const supabaseAnonKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVwc3p4cmt0YWxwdmtsZnZocW5rIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg0NjE4NjksImV4cCI6MjEwNDAzNzg2OX0.61CtzTiyalDLJ8BvrRYTzOVob358rU4mcjPXGOREKyI";
 
